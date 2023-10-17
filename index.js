d32f70164912b9be0bcf97a8e6260bc548cfed35
Paulo Ricardo <paulo.nazario5436@gmail.com>
2023-10-17 13:19:32 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+// Defina o nome e a XP do herói
+var nomeDoHeroi = "Paulpy"; // Substitua pelo nome do herói
+var xpDoHeroi = 6000; // Substitua pela quantidade de XP do herói
+
+// Use uma estrutura de decisão para determinar o nível do herói
+var nivelDoHeroi;
+if (xpDoHeroi < 1000) {
+    nivelDoHeroi = "Ferro";
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+    nivelDoHeroi = "Bronze";
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+    nivelDoHeroi = "Prata";
+} else if (xpDoHeroi >= 6001 && xpDoHeroi <= 7000) {
+    nivelDoHeroi = "Ouro";
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+    nivelDoHeroi = "Platina";
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+    nivelDoHeroi = "Ascendente";
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+    nivelDoHeroi = "Imortal";
+} else {
+    nivelDoHeroi = "Radiante";
+}
+
+// Exiba a mensagem com o nome e o nível do herói
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivelDoHeroi}`);
